d044a717ff1979e0b08735f17df63a62b8352384
Ryazhenka <dimaorynchukk@gmail.com>
2025-10-19 19:02:19 +0300

    🔥 Полная зачистка Ultrahand: переименования, фиксы типов, удаление конфликтов

---
+++ build/debug_funcs.d
@@ -0,0 +1,4 @@
+debug_funcs.o: \
+ C:/Users/dimas/Ryazhahand-Overlay/lib/libRYAZHAHAND/libultra/source/debug_funcs.cpp \
+ C:/Users/dimas/Ryazhahand-Overlay/lib/libRYAZHAHAND/libultra/include/debug_funcs.hpp
+C:/Users/dimas/Ryazhahand-Overlay/lib/libRYAZHAHAND/libultra/include/debug_funcs.hpp:
