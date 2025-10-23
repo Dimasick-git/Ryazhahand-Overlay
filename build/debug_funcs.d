3738834b04a8c4ed9eb6a9489a4589e98ccb1452
Dimasick-git <dimaorynchukk@gmail.com>
2025-10-23 22:41:48 +0300

    Delete build directory

--- build/debug_funcs.d
+++
@@ -1,4 +0,0 @@
-debug_funcs.o: \
- C:/Users/dimas/Ryazhahand-Overlay/lib/libRYAZHAHAND/libultra/source/debug_funcs.cpp \
- C:/Users/dimas/Ryazhahand-Overlay/lib/libRYAZHAHAND/libultra/include/debug_funcs.hpp
-C:/Users/dimas/Ryazhahand-Overlay/lib/libRYAZHAHAND/libultra/include/debug_funcs.hpp:
